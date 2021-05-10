9d9055dadb39b8c1ec6db46021c345ce79b74b43
AngieD8615 <adavidson1@gmail.com>
2021-05-10 13:14:03 -0600

    refactored so that the input for posting a movie is a title

---
+++ src/main/java/com/adavidson/movies/Movie.java
@@ -0,0 +1,37 @@
+package com.adavidson.movies;
+
+public class Movie {
+    private int id;
+    private String title;
+
+    public Movie() {
+    }
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/adavidson/movies/MovieController.java
@@ -0,0 +1,25 @@
+package com.adavidson.movies;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    DataService dataService;
+
+    public MovieController(DataService dataService) {
+        this.dataService = dataService;
+    }
+
+    @GetMapping()
+    public List<Movie> getMovies () {
+        return dataService.getMovies();
+    }
+    @PostMapping()
+    public Movie addMovie (@RequestBody String title) {
+        Movie newMovie = new Movie(title);
+        return dataService.addMovie(newMovie);
+    }
+}
